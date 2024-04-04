d4fad08e0301aca2e3020dddccf6effba5473138
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-04-04 08:29:07 +0700

    docs: access data from request

--- 02.messages/src/messages/messages.controller.ts
+++ 02.messages/src/messages/messages.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
   @Get() // Tạo request với method GET
   listMessages() {}
   @Post() // Tạo request với method POST
-  createMessage() {}
+  createMessage(@Body() body: any) { // Body là nội dung của request POST
+    console.log(body); 
+  }
   @Get('/:id') // Tạo request với method GET và id là params của request
-  getMessage() {}
+  getMessage(@Param('id') id: string) { // Param là params của request
+    console.log(id);
+  }
 }
