444ee4d3986a7a2f1c422163f764b553cd2f2c5f
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-04-04 11:16:33 +0700

    docs: use repo in service, use service in controller

--- 02.messages/src/messages/messages.repository.ts
+++ 02.messages/src/messages/messages.repository.ts
@@ -4,24 +4,24 @@ export class MessageRepository {
   // Repository của message
   async findOne(id: string) {
     // Hàm tìm kiếm message theo id
-    const data = await readFile('../../data/messages.json', 'utf-8'); // Đọc file messages.json
+    const data = await readFile('messages.json', 'utf-8'); // Đọc file messages.json
     const message = JSON.parse(data); // Parse dữ liệu đọc được
     return message[id]; // Trả về message theo id
   }
 
   async findAll() {
     // Hàm lấy tất cả message
-    const data = await readFile('../../data/messages.json', 'utf-8'); // Đọc file messages.json
+    const data = await readFile('messages.json', 'utf-8'); // Đọc file messages.json
     const message = JSON.parse(data); // Parse dữ liệu đọc được
     return message; // Trả về tất cả message
   }
 
   async create(content: string) {
     // Hàm tạo message mới
-    const data = await readFile('../../data/messages.json', 'utf-8'); // Đọc file messages.json
+    const data = await readFile('messages.json', 'utf-8'); // Đọc file messages.json
     const message = JSON.parse(data); // Parse dữ liệu đọc được
     const id = Math.floor(Math.random() * 999); // Tạo id ngẫu nhiên
     message[id] = { content, id }; // Thêm message mới vào object message
-    await writeFile('../../data/messages.json', JSON.stringify(message)); // Ghi lại file messages.json
+    await writeFile('messages.json', JSON.stringify(message)); // Ghi lại file messages.json
   }
 }
