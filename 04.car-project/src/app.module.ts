5710c1532a2c2b80238eef18cba00cc9d30b8b36
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-04-10 16:40:28 +0700

    feat: Create connection with sqlite

--- 04.car-project/src/app.module.ts
+++ 04.car-project/src/app.module.ts
@@ -4,8 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 
+// Import TypeORM modules để sử dụng database
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // Loại database
+      database: 'db.sqlite', // Tên database sẽ tạo
+      entities: [], // Thêm các entity vào đây để sử dụng
+      synchronize: true, // Tự động tạo bảng trong database khi chạy ứng dụng (Chỉ sử dụng trong môi trường dev)
+    }), // Nhập import TypeOrmModule.forRoot({}) để sử dụng databas, tạo file database ở ngay bên ngoài thư mục src
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
