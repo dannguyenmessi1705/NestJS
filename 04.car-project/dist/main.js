28ae6c09bf1bd5f032e837b8bea7657cf1243ea7
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-04-17 02:02:12 +0700

    docs: Cookie Session in NestJS

--- 04.car-project/dist/main.js
+++ 04.car-project/dist/main.js
@@ -1,10 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const cookie_session_1 = __importDefault(require("cookie-session"));
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use((0, cookie_session_1.default)({
+        keys: ['dan17052002'],
+    }));
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
     }));
--- 04.car-project/src/main.ts
+++ 04.car-project/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; // Import ValidationPipe từ @nestjs/common để sử dụng pipe
+import cookieSession from 'cookie-session'; // Import cookieSession từ 'cookie-session' để sử dụng cookie session
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['dan17052002'],
+    }),
+  ); // Sử dụng cookie session với key là 'dan17052002'
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Bật whitelist để loại bỏ các trường không cần thiết trong DTO nếu người dùng gửi thêm
