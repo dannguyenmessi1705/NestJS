82d56a9da780d820a0797acd90082f30cd99e19b
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-04-19 21:31:35 +0700

    update: entity of report

--- 04.car-project/src/reports/reports.entity.ts
+++ 04.car-project/src/reports/reports.entity.ts
@@ -2,10 +2,27 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'; //Import các 
 
 @Entity() // Đánh dấu class này là một entity
 export class Report {
-    @PrimaryGeneratedColumn() // Đánh dấu cột này là primary key và tự động tăng
-    id: number;
+  @PrimaryGeneratedColumn() // Đánh dấu cột này là primary key và tự động tăng
+  id: number;
 
-    @Column() // Đánh dấu cột này là một cột trong database
-    price: number;
+  @Column() // Đánh dấu cột này là một cột trong database
+  price: number;
 
+  @Column()
+  make: string;
+
+  @Column()
+  modal: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  mileage: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  lng: number;
 }
