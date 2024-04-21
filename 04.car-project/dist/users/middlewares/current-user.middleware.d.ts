ab107201f4443ed599e4483f8d0f987c1f736d81
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-04-21 23:37:13 +0700

    NOTE: declare global namespace interface

--- 04.car-project/dist/users/middlewares/current-user.middleware.d.ts
+++ 04.car-project/dist/users/middlewares/current-user.middleware.d.ts
@@ -1,6 +1,14 @@
 import { NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../users.entity';
+declare global {
+    namespace Express {
+        interface Request {
+            user?: User;
+        }
+    }
+}
 export declare class CurrentUserMiddleware implements NestMiddleware {
     private userService;
     constructor(userService: UsersService);
--- 04.car-project/src/users/middlewares/current-user.middleware.ts
+++ 04.car-project/src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../users.entity';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+} // Khai báo global namespace Express để mở rộng interface Request, thêm thuộc tính user kiểu User
+// Để có thể sủe dụng req.user mà không báo lỗi (Không phải dùng //@ts-ignore)
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private userService: UsersService) {}
@@ -8,7 +18,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
     const { user_id } = req.session || {}; // Lấy user_id từ session trong request
     if (user_id) {
       const user = await this.userService.getUserById(user_id); // Lấy user từ user_id
-      //@ts-ignore
       req.user = user; // Gán user vào request
     }
     next(); // Chuyển tiếp request sang middleware tiếp theo
